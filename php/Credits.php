d999c6cc59f18afbd808b16c56fe5cae417e596a
Jon Ander Rodriguez <jrodriguez151@ikasle.ehu.es>
2022-06-09 11:29:28 +0200

    Agregar Imagen Info

--- php/Credits.php
+++ php/Credits.php
@@ -8,7 +8,14 @@
   <section class="main" id="s1">
     <div>
 
-      <h2>DATOS DEL AUTOR/AUTORES</h2>
+      <h3>Aplicación desarrollada por Jon Ander Rodríguez</h3>
+      <h3>Prin-gao Informatico y Estudiante de la Facultad de Informática de la UPV/EHU</h3>
+      <h3>Especialización en Ingeniería de Computadores</h3>
+      <br>
+      <img src="../images/avatar.jpg" style="width:250px; height:246px">
+      <br>
+      <br>
+      <h5>Plantilla de proyecto aportada por José Ángel Vadillo (Docente)</h5>
 
     </div>
   </section>
--- php/QuestionForm.php
+++ php/QuestionForm.php
@@ -6,11 +6,44 @@
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
-    <div>
+    <div aling="left"> 
+      <form id='fquestion' name='fquestion' action='AddQuestion.php'>
 
-      Añadir el formulario y los scripts necesarios para que el usuario <br>
-      pueda introducir los datos de una pregunta sin imagen.
+        <p>
+          <label for='email'> Dirección de Correo: </label>
+          <input type='text' id='email' name='email'>
+        </p>
+        <p>
+          <label for='question'> Enunciado de la pregunta: </label>
+          <input type='text' id='question' name='question'>
+        </p>
+        <p>
+          <label for='resp_ok'> Respuesta correcta: </label>
+          <input type='text' id='resp_ok' name='resp_ok'>
+        </p>
+        <p>
+          <label for='resp_mal1'> Respuesta incorrecta 1: </label>
+          <input type='text' id='resp_mal1' name='resp_mal1'>
+        </p>
+        <p>
+          <label for='resp_mal2'> Respuesta incorrecta 2: </label>
+          <input type='text' id='resp_mal2' name='resp_mal2'>
+        </p>
+        <p>
+          <label for='resp_mal3'> Respuesta incorrecta 3: </label>
+          <input type='text' id='resp_mal3' name='resp_mal3'>
+        </p>
+        <p>
+          <label for='tema'> Tema: </label>
+          <input type='text' id='tema' name='tema'>
+        </p>
+        <p>
+          <label for='complejidad'> Complejidad: </label>
+          <input type='text' id='complejidad' name='complejidad'>
+        </p>
+        <input type='submit' value='preguntar' id='preguntar' onclick='return validarForm();'>
 
+      </form>
     </div>
   </section>
   <?php include '../html/Footer.html' ?>
